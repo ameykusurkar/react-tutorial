321e78907d45a2e2e360aafd075f958af61cde7a
Amey Kusurkar <amey1000@gmail.com>
2017-07-31 00:01:44 +0100

    Setup a basic board

--- src/App.js
+++ src/App.js
@@ -5,29 +5,48 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Board />
     );
   }
 }
 
 class Square extends Component {
   render() {
-    return <div>Square</div>
+    return (
+      <button className="square"></button>
+    );
+  }
+}
+
+class Board extends Component {
+  renderSquare() {
+    return <Square />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+        <div className="board-row">
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+        <div className="board-row">
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+      </div>
+    );
   }
 }
 
 class Game extends Component {
-}
-
-class Board extends Component {
 }
 
 export default App;
