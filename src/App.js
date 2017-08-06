2310b6b0ef04e300c13c44611e6995a56e135bdb
Amey Kusurkar <amey1000@gmail.com>
2017-08-06 23:04:00 +0100

    Pull Square state up into Board

--- src/App.js
+++ src/App.js
@@ -13,14 +13,33 @@ class App extends Component {
 class Square extends Component {
   render() {
     return (
-      <button className="square"></button>
+      <button className="square"
+        onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
     );
   }
 }
 
 class Board extends Component {
-  renderSquare() {
-    return <Square />;
+  constructor() {
+    super();
+    this.state = { squares: Array(9).fill(null) };
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
   }
 
   render() {
@@ -30,19 +49,19 @@ class Board extends Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
